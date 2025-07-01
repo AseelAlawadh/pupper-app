8a164ddc58a0067fdf2bad81f3cbe071c7d5e5b8
aseelalawadh <aseel.alawadh@gmail.com>
2025-06-30 22:21:59 -0400

    init

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-
+from cdk_nag import AwsSolutionsChecks
 from cdk_pupper.cdk_pupper_stack import CdkPupperStack
 
 
 app = cdk.App()
+# Apply CDK Nag compliance checks globally
+cdk.Aspects.of(app).add(AwsSolutionsChecks())
 CdkPupperStack(app, "CdkPupperStack")
 
 app.synth()
